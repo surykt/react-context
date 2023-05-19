3491df6764eea806a3b5f224975a2b34297b2abf
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-05-19 16:56:40 -0300

    create carrinho context

--- src/pages/Feira/index.js
+++ src/pages/Feira/index.js
@@ -13,7 +13,7 @@ function Feira() {
       <Header>
         <div>
           <h2>Olá! {nome}</h2>
-          <h3>Saldo: R$ {saldo} </h3>
+          <h3>Saldo: R$ {saldo} Reais</h3>
         </div>
         <p>Encontre os melhores produtos orgânicos!</p>
       </Header>
